d81d628aab44f781681f507ba54048b7b5fb03b4
Alex62510 <eagle625@tut.by>
2023-08-08 13:33:52 +0300

    add all styles input default

---
+++ src/components/ui/textFeld/textField.stories.tsx
@@ -0,0 +1,70 @@
+import { useState } from 'react'
+
+import { Meta } from '@storybook/react'
+
+import { TextField } from '../textFeld/textField.tsx'
+
+import { Eye } from '@/assets/icons/eye.tsx'
+
+export default {
+  title: 'Components/Data Entry/Text Field',
+  component: TextField,
+} as Meta<typeof TextField>
+
+export const Primary = {
+  args: {
+    label: 'Input',
+    placeholder: 'Input',
+    error: false,
+    disabled: false,
+  },
+}
+export const WithIcon = {
+  args: {
+    label: 'Input',
+    placeholder: 'Input',
+    iconEnd: <Eye fill={'var(--color-dark-100)'} />,
+    error: false,
+    disabled: false,
+  },
+}
+
+export const Search = {
+  args: {
+    placeholder: 'Input search',
+    search: true,
+    error: false,
+    disabled: false,
+  },
+}
+
+export const SearchChangeValue = {
+  render: () => {
+    const [text, setText] = useState('')
+
+    return (
+      <>
+        <TextField
+          placeholder={'Input search'}
+          search={true}
+          value={text}
+          onChange={e => setText(e.currentTarget.value)}
+          onClearClick={() => setText('')}
+        />
+      </>
+    )
+  },
+  args: {
+    label: 'Some label',
+    placeholder: 'Search...',
+    search: true,
+  },
+}
+
+export const Invalid = {
+  render: () => {
+    return (
+      <TextField value="some value" label="some label" error={true} errorMessage="Error mesage" />
+    )
+  },
+}
--- src/components/ui/textFeld/textField.tsx
+++ src/components/ui/textFeld/textField.tsx
@@ -2,15 +2,18 @@ import { ComponentProps, forwardRef, ReactNode, KeyboardEvent } from 'react'
 
 import { clsx } from 'clsx'
 
-import { Search as SearchIcon, Typography } from '../../../assets/icons/search.tsx'
-import { Close } from '../../assets/icons'
-import { Label } from '../label'
+import { Search as SearchIcon } from '../../../assets/icons/search.tsx'
+import { Label } from '../label/label.tsx'
+import { Typography } from '../typography/typography.tsx'
 
 import s from './textField.module.scss'
+
+import { Close } from '@/assets/icons/Close.tsx'
 
 export type TextFieldProps = {
   value?: string
   label?: ReactNode
+  error?: boolean
   errorMessage?: string
   iconStart?: ReactNode
   iconEnd?: ReactNode
@@ -39,7 +42,7 @@ export const TextField = /* @__PURE__ */ forwardRef<HTMLInputElement, TextFieldP
     const showError = !!errorMessage && errorMessage.length > 0
 
     if (search) {
-      iconStart = <SearchIcon size={20} color={'var(--color-text-secondary)'} />
+      iconStart = <SearchIcon width={20} height={20} fill={'var(--color-dark-100)'} />
     }
     const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
       if (onEnter && e.key === 'Enter') {
@@ -78,7 +81,7 @@ export const TextField = /* @__PURE__ */ forwardRef<HTMLInputElement, TextFieldP
             />
             {isShowClearButton && (
               <button className={classNames.clearButton} onClick={onClearClick} type="button">
-                {<Close size={20} color={'var(--color-border-input-active)'} />}
+                {<Close width={20} height={20} color={'var(--color-info-500)'} />}
               </button>
             )}
             {!!iconEnd && <span className={classNames.iconEnd}>{iconEnd}</span>}
